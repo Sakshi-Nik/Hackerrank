bef25e0f8d9670efc8c5066ad7dc41e09d7242dc
Sakshi-Nik <sakshinikam266@gmail.com>
2026-03-28 16:25:51 +0530

    Day81

---
+++ day81.py
@@ -0,0 +1,15 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+# Read number of test cases
+t = int(input())
+
+for _ in range(t):
+    
+    n_a = int(input())
+    set_a = set(map(int, input().split()))
+    
+    
+    n_b = int(input())
+    set_b = set(map(int, input().split()))
+    
+    
+    print(set_a.issubset(set_b))
