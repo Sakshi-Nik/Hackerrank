c2fb757f5bf281c57e4391292981133adb4fc9f7
Sakshi-Nik <sakshinikam266@gmail.com>
2026-03-31 20:20:27 +0530

    Day84

---
+++ day84.py
@@ -0,0 +1,19 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from collections import defaultdict
+
+
+n, m = map(int, input().split())
+
+d = defaultdict(list)
+
+
+for i in range(1, n + 1):
+    word = input()
+    d[word].append(i)
+
+for _ in range(m):
+    word = input()
+    if word in d:
+        print(*d[word])
+    else:
+        print(-1)
