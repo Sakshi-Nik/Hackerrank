d62dc7202b1f2dc7af34af4290dc60d025cdbd98
Sakshi-Nik <sakshinikam266@gmail.com>
2026-04-08 17:54:34 +0530

    Day92

---
+++ day92.py
@@ -0,0 +1,10 @@
+import numpy
+
+def arrays(arr):
+    # complete this function
+    # use numpy.array
+    
+    return numpy.array(arr, dtype=float)[::-1]
+arr = input().strip().split(' ')
+result = arrays(arr)
+print(result)
