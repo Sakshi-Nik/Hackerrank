6145d2981d57ab86c1754f7014587c3c493eabb0
Sakshi-Nik <sakshinikam266@gmail.com>
2026-04-09 18:48:09 +0530

    Day93

---
+++ day93.py
@@ -0,0 +1,6 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from __future__ import print_function
+
+var = raw_input() if hasattr(__builtins__, 'raw_input') else input()
+
+eval(var)
