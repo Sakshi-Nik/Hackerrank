4d82417527673c0d16606885c71a20517ae12a0f
Sakshi-Nik <sakshinikam266@gmail.com>
2026-04-10 20:30:16 +0530

    Day94

---
+++ day94.py
@@ -0,0 +1,12 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import numpy
+n,m=map(int,input().split())
+a = numpy.array([input().split() for _ in range(n)], int)
+b = numpy.array([input().split() for _ in range(n)], int)
+
+print (numpy.add(a, b) )
+print (numpy.subtract(a, b) )
+print (numpy.multiply(a, b) )
+print(numpy.floor_divide(a, b))
+print (numpy.mod(a, b) )
+print (numpy.power(a, b) )
